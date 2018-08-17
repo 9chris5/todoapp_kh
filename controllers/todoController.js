e89d119fb56572558be7c5ea0689206bd09fa7f7
chris <kheciek@gmail.com>
2018-08-17 22:39:18 +0200

    final commit

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -15,21 +15,21 @@ var urlencodedParser = bodyParser.urlencoded({extended: false});
 
 module.exports = function(app){
 
-  app.get('/todo', function(req, res){
+  app.get('/', function(req, res){
     Todo.find({}, function(err, data){
       if (err) throw err;
       res.render('todo', {todos: data});
     });
   });
 
-  app.post('/todo', urlencodedParser, function(req, res){
+  app.post('/', urlencodedParser, function(req, res){
     var newTodo = Todo(req.body).save(function(err, data){
       if (err) throw err;
       res.json(data);
     });
   });
 
-  app.delete('/todo/:item', function(req, res){
+  app.delete('/:item', function(req, res){
     Todo.find({item: req.params.item.replace(/\-/g, " ")}).remove(function(err, data){
       if (err) throw err;
       res.json(data);
